20c32f0c045d6cd2a793aaf38638b8b89b4c2718
quangthaiifi <nqthaicntttn@gmail.com>
2024-06-02 22:51:19 +0700

    first commit

---
+++ test/pageobjects/HomePage.js
@@ -0,0 +1,7 @@
+const { $ } = require('@wdio/globals')
+const Page = require('./page');
+class HomePage extends Page{
+
+    
+
+}
---
+++ test/pageobjects/page.js
@@ -0,0 +1,26 @@
+const { browser } = require('@wdio/globals')
+
+/**
+* main page object containing all methods, selectors and functionality
+* that is shared across all page objects
+*/
+module.exports = class Page {
+    /**
+    * Opens a sub page of the page
+    * @param path path of the sub page (e.g. /path/to/page.html)
+    */
+    open () {
+        browser.url(`https://www.thegioididong.com/`)
+        browser.maximizeWindow();
+        return
+    }
+    clickToText(text){
+            $("//*[text()='" + text + "']").click();
+    }
+    clickToElement(locator){
+            $(locator).click()
+    }
+    getText(locator){
+        return $(locator).getText()
+    }
+}
